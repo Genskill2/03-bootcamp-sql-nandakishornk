054ff008ccbc760121d24fab824d3f8dd857253f
Nandakishor <nandakishornk01@gmail.com>
2021-06-16 23:15:54 +0530

    edit

--- create.sql
+++ create.sql
@@ -1,21 +1,21 @@
-CREATE TABLE publisher{
+CREATE TABLE publisher(
 	id INTEGER PRIMARY KEY,
 	name TEXT,
 	country TEXT
-};
+);
 
-CREATE TABLE books{
+CREATE TABLE books(
 	id INTEGER PRIMARY KEY,
 	title TEXT,
 	publisher INT REFERENCES publisher(id)
-};
+);
 
-CREATE TABLE subjects{
+CREATE TABLE subjects(
 	id INTEGER PRIMARY KEY,
 	name TEXT
-};
+);
 
-CREATE TABLE books_subjects{
+CREATE TABLE books_subjects(
 	books INTEGER REFERENCES books(id),
 	subjects INTEGER REFERENCES subjects(id)
-};
+);
